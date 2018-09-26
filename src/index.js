0b04714d24c361efc6ae1dd07d5aa18568bb8ba9
Gareth ODuffy <goduffy22@gmail.com>
2018-09-26 13:10:36 +0100

    Final working connect_four

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Column, Game1, Square, Board} from './connect.js';
-import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<Board />, document.getElementById('root'));
-//registerServiceWorker();
+
