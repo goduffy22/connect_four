406ce1a4a93a59c7c56546ad4bef71ff99e3a960
Gareth ODuffy <goduffy22@gmail.com>
2018-09-26 16:05:27 +0100

    Final connect_four without unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Column, Game1, Square, Board} from './connect.js';
+import {Board} from './connect.js';
 
 
 ReactDOM.render(<Board />, document.getElementById('root'));
